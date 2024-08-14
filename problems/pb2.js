5b59f55f4781669114767bd4bca6bcdf3e2b5a61
BabySasukeNotNaruto <98043284+BabySasukeNotNaruto@users.noreply.github.com>
2024-08-13 19:33:14 -0500

    Update pb2.js

--- problems/pb2.js
+++ problems/pb2.js
@@ -1,6 +1,28 @@
 // declare and define function here
+function mostFrequentChar(str) 
+  const frequency = {};
+  for (let char of str) {
+    if (frequency[char]) {
+        frequency[char]++;
+    } else {
+      frequency[char] = 1;
+    }
+  }
+
+  let maxFreq - 0;
+  let mostFrequent = '';
+
+  for (let char of str) {
+    if (frequency[char] > maxFreq) {
+      maxFreq = frequency[char];
+      mostFrequent[char] = 1;
+    }
+  }
+
+return mostFrequent;
+}
 
 
 // test cases
-// mostFrequentChar('bookeeper'); // -> 'e'
-// mostFrequentChar('mississippi'); // -> 'i'. 'i' and 's' appear with same frequency in 'mississippi' but 'i' appears first!
+mostFrequentChar('bookeeper'); // -> 'e'
+mostFrequentChar('mississippi'); // -> 'i'. 'i' and 's' appear with same frequency in 'mississippi' but 'i' appears first!
